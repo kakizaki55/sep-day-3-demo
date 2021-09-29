eb392ce1b2405b70586b1debda548e561ec40fca
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-09-29 11:04:52 -0700

    just some notes for intro to javascript

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-// import functions and grab DOM elements
 
-// initialize global state
+console.log('hello world');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+console.log(1 + 1);
+
+console.log('true');
+
+console.log(true);
+
+let myString = true;
+
+console.log(myString);
+
+console.log(typeof myString);
+
+const myArray = ['green', 'purple', 'red'];
+
+console.log(myArray);
+
+const theColorGreen = myArray[0];
+
+console.log(theColorGreen);
+
+myArray[0] = 'pink';
+
+console.log(theColorGreen);
+
+
+console.log(myArray);
